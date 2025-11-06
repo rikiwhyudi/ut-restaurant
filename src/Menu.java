f15faaf01ec8737287199a971c548fdf82beb3ea
rikiwhyudi <riki.whyudi45@gmail.com>
2025-11-06 15:19:47 +0700

    refactor(menu, main): standardize index validation, add cancel option and srp addProduct method

--- src/Menu.java
+++ src/Menu.java
@@ -1,40 +1,66 @@
 public class Menu {
     private Product[] products = new Product[0];
 
-    public void addProduct(Product product) {
+    private boolean isDuplicate(Product product) {
         for (Product p : products) {
             if (p.getProductName().equalsIgnoreCase(product.getProductName())) {
-                System.out.println("Produk sudah ada di daftar.");
-                return;
+                return true;
             }
         }
+        return false;
+    }
+
+    private int getInsertIndex(Product product) {
+        int index = products.length;
+
+        // jika makanan, cari dimana kategori makanan berakhir
+        if ("Makanan".equalsIgnoreCase(product.getCategory())) {
+            index = 0;
+            // lakukan traversal selama masih melihat katogori makanan
+            while (index < products.length && "Makanan".equalsIgnoreCase(products[index].getCategory())) {
+                index++;
+            }
+        }
+        return index;
+    }
+
+    private Product[] insertIntoArray(Product product, int insertIndex) {
 
         Product[] newArr = new Product[products.length + 1];
-        int insertIndex = 0;
 
-        if ("Makanan".equalsIgnoreCase(product.getCategory())) {
-            while (insertIndex < products.length && "Makanan".equalsIgnoreCase(products[insertIndex].getCategory())) {
-                insertIndex++;
-            }
-        } else {
-            insertIndex = products.length;
-        }
-
+        // salin semua elemen dari index 0 hingga sebelum insertIndex
         for (int i = 0; i < insertIndex; i++) {
             newArr[i] = products[i];
         }
 
+        // sisipkan item baru di posisi yang sudah ditentukan.
         newArr[insertIndex] = product;
 
+        // salin elemen dari insertIndex array lama ke posisi (i + 1) array baru.
         for (int i = insertIndex; i < products.length; i++) {
             newArr[i + 1] = products[i];
         }
-        products = newArr;
+
+        return newArr;
     }
 
-    // cek apakah index invalid
+    // cek apakah indexnya invalid
     private boolean isInvalidIndex(int index) {
         return index < 0 || index >= products.length;
+    }
+
+    public void addProduct(Product product) {
+        // cek apakah produk sudah ada di daftar
+        if (isDuplicate(product)) {
+            System.out.println("Produk sudah ada di daftar.");
+            return;
+        }
+
+        // tentukan posisi penyisipan product
+        int insertIndex = getInsertIndex(product);
+
+        // sisipkan dan ganti referensi ke array baru
+        products = insertIntoArray(product, insertIndex);
     }
 
     public void removeProduct(int index) {
